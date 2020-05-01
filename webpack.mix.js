536b51b312037154390255d3e4663ac306bc138d
Don Jones <newelementdesigns@me.com>
2020-04-30 22:12:12 -0400

    bumped deps

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.options({ processCssUrls: false })
-.sass('resources/assets/sass/shoppe.scss', 'publishable/assets/css', { implementation: require('node-sass') })
+.sass('resources/assets/sass/shoppe.scss', 'publishable/assets/css')
 .js('resources/assets/js/shoppe.js', 'publishable/assets/js').sourceMaps();
